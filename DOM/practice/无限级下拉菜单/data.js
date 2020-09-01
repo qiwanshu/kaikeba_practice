8a8d3d419e6f0d0cad47ef6eaaf8f7fe13fd71cf
QWS1101 <yangyang_daisy@yeah.net>
2020-09-01 10:59:28 +0800

    DOM

---
+++ DOM/practice/无限级下拉菜单/data.js
@@ -0,0 +1,82 @@
+// 扁平化数据结构
+var data = [
+	{
+		id:0,  
+		pid:-1,
+		title:"微云",
+	},
+	// {
+	// 	id:100,  
+	// 	pid:-1,
+	// 	title:"哈哈哈",
+	// },
+	{
+		id:1,
+		pid:0,
+		title:"我的文档"
+	},
+	{
+		id:2,
+		pid:0,
+		title:"我的音乐"
+	},
+	{
+		id:2999,
+		pid:1,
+		title:"js程序设计"
+	},
+	{
+		id:29000,
+		pid:1,
+		title:"js权威指南"
+	},
+	{
+		id:23333,
+		pid:2,
+		title:"王杰"
+	},
+	{
+		id:244444,
+		pid:2,
+		title:"张国荣"
+	},
+	{
+		id:3,
+		pid:2,
+		title:"周杰伦"
+	},
+	{
+		id:4000,
+		pid:3,
+		title:"稻香"
+	},
+	{
+		id:4,
+		pid:3,
+		title:"发如雪"
+	},
+	{
+		id:600,
+		pid:3,
+		title:"夜曲"
+	}
+]
+
+
+const data2 = [
+	{
+		id:0,  
+		pid:-1,
+		title:"微云",
+	},
+	{
+		id:4,
+		pid:0,
+		title:"发如雪"
+	},
+	{
+		id:18888,  
+		pid:-1,
+		title:"微云2",
+	},
+]
